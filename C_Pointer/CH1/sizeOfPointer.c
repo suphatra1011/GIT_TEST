425aa470eaebe20506d3f5889b83a4a66caf9641
suphatra1011 <suphatra1011@gmail.com>
2020-08-14 16:24:08 +0800

    Create sizeOfPointer.c

---
+++ C_Pointer/CH1/sizeOfPointer.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+    char *a; 
+    short *b; 
+    int *c; 
+    long int *d; 
+    float *e; 
+    double *f;
+    printf("sizeof char*: %lu\n", sizeof(a));
+    printf("sizeof short*: %lu\n", sizeof(b));
+    printf("sizeof int*: %lu\n", sizeof(c));
+    printf("sizeof long int*: %lu\n", sizeof(d));
+    printf("sizeof float*: %lu\n", sizeof(e));
+    printf("sizeof double*: %lu\n", sizeof(f));
+    
+    return 0;
+}
